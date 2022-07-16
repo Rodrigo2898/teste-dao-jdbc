e5d717fc2a71ef0b71f824cc3a74a11228ec3fed
Rodrigo Feitosa <rodrigofeitosarodrigues@gmail.com>
2022-07-16 20:52:24 -0300

    findById Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,16 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Books");
-		
-		Seller seller = new Seller(23, "Rod", "rod@gmail.com", new Date(), 3000.0, obj);
-		
-		//Dessa forma o programa não conhece a implementação, ele conhece somente a interface
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 	}
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,5 +1,6 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 //Classe auxiliar reponsável por instanciar os meus Daos
@@ -7,6 +8,6 @@ public class DaoFactory {
 
 	//"Macete", para não precisar expor a implementação
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 }
