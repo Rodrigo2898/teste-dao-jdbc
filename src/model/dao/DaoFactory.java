10e5095746d9a6e8f4b26d6afad881c10d414502
Rodrigo Feitosa <rodrigofeitosarodrigues@gmail.com>
2022-07-17 21:56:27 -0300

    DepartmentDao implementation and test

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 //Classe auxiliar reponsável por instanciar os meus Daos
@@ -9,5 +10,9 @@ public class DaoFactory {
 	//"Macete", para não precisar expor a implementação
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
 	}
 }
